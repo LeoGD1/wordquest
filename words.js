f702660e707f8e43de5380dfea5a17a0fa7afa77
LeoGD1 <bridonegd@outlook.com>
2024-09-13 22:06:29 -0600

    se eliminan posibles acentos

--- words.js
+++ words.js
@@ -5,7 +5,7 @@ const words = [
     "sola", "taza", "vela", "vino", "bajo", "boca", "cara", "dato", "feliz", "frio",
     "hilo", "jugo", "lago", "lima", "miel", "moño", "nube", "pepe", "pico", "rama",
     "rojo", "ruma", "seda", "taco", "tapa", "toro", "vaca", "vela", "vino", "zeta",
-    "yate", "bote", "bola", "cafe", "duro", "río", "moto", "nana", "puro", "palo",
+    "yate", "bote", "bola", "cafe", "duro", "rio", "moto", "nana", "puro", "palo",
     "silla", "tira", "cola", "moto", "fijo", "nada", "rima", "fruta", "oro", "piso",
     "auto", "rosa", "luna", "fiel", "ruido", "verde", "nuevo", "nube", "foto", "nube"
 ];
